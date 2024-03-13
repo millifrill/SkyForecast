1be24324ed5ea65b7ea6f8ef8062147316a30317
Camilla <73700387+millifrill@users.noreply.github.com>
2024-03-13 06:29:59 +0100

    added ThemeContext

---
+++ src/context/ThemeContext.jsx
@@ -0,0 +1,35 @@
+import { createContext, useEffect, useState } from 'react';
+
+const themes = {
+	dark: {
+		backgroundColor: '#242424',
+		color: 'white',
+	},
+	light: {
+		backgroundColor: 'white',
+		color: 'black',
+	},
+};
+
+export const ThemeContext = createContext(null);
+
+export const ThemeProvider = ({ children }) => {
+	const [isDark, setIsDark] = useState(false);
+	const [theme, setTheme] = useState(themes.light);
+
+	const toggleTheme = () => {
+		setIsDark((prevIsDark) => !prevIsDark);
+	};
+
+	useEffect(() => {
+		setTheme(isDark ? themes.dark : themes.light);
+	}, [isDark]);
+
+	return (
+		<ThemeContext.Provider value={{ theme, isDark, toggleTheme }}>
+			{children}
+		</ThemeContext.Provider>
+	);
+};
+
+export const useTheme = () => useContext(ThemeContext);
